567312459cc8105850d2b783e6f991f659eade35
ineszz <ines.asandei@gmail.com>
2020-12-25 00:38:46 +0200

    init

---
+++ docs/R/New-post.R
@@ -0,0 +1,16 @@
+distill::create_post(title = "Final de sezon",
+                     author = "Bucharest R-Ladies",
+                     date_prefix = TRUE)
+
+distill::create_post(title = "Primul panel al comunitatii",
+                     author = "Bucharest R-Ladies",
+                     date_prefix = TRUE)
+#2020-09-30-
+
+distill::create_post(title = "meetup12: Text mining with R, Speaker Aurore Paligot",
+                     author = "Bucharest R-Ladies",
+                     date_prefix = TRUE)
+
+distill::create_post(title = "meetup13: Ana-Maria Niculescu & Frie Preu despre R, git, Github & Raspberry Pi",
+                     author = "Bucharest R-Ladies",
+                     date_prefix = TRUE)
